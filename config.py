3e41869ac7982747c17e67d96c929dfe0467dd13
thyagomota <omota@mines.edu>
2014-09-29 14:31:33 -0600

    2nd commit

---
+++ config.py
@@ -0,0 +1,7 @@
+ACCEPTED_FILE_TYPES = ['ASCII C program text', 'ASCII C program text, with CRLF line terminators', 'ASCII C program text, with CRLF, LF line terminators', 'ASCII C++ program text', 'ASCII C++ program text, with CRLF line terminators', 'ASCII C++ program text, with CRLF, LF line terminators']
+COLORS = ['#F78181', '#D8F781', '#58FAAC', '#FFBF00', '#04B486', '#A9F5BC', '#BCA9F5', '#F6CEEC', '#00FF40', '#0174DF']
+SOURCE_FILE_EXTENSIONS  = ['.cpp', '.c', '.hpp', '.h']
+TEMP_FOLDER             = 'temp'
+ORIGINAL_FILE_NAME      = 'source'
+FORMATTED_FILE_NAME     = ORIGINAL_FILE_NAME + '_new'
+MAX_SCORE               = 2
